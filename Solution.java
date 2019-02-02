b79b2157ba2114cddcf60a8fe8bd7957ff82c74e
sdim-ua <ingersol@yandex.ua>
2019-02-03 00:05:36 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        
+         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int num = Integer.parseInt(reader.readLine());;
+        
+        while (num !=0){
+            
+            if (num % 2 == 0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+            num = num / 10;
+            
+        }
+            
+       System.out.println( "Even: " + even + " " + "Odd: " + odd);    
+    }
+}
